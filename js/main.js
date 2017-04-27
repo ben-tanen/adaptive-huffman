94954b404c21c928d57940d694c7cce3ffdc4fb2
Ben Tanen <ben.tanen@gmail.com>
2017-04-27 18:45:12 -0400

    updated styling (vertical tabs instead of horizontal)

--- js/main.js
+++ js/main.js
@@ -3,21 +3,25 @@ $(document).ready(function() {
         var sibs = $('.page')
         var ix   = $(this).parent().index();
 
-        if ($(this).parent().hasClass('closed')) {
+        $('.tab.top').removeClass('top');
+        $(sibs[ix]).find('.tab').addClass('top');
+
+        if ($(sibs[ix]).hasClass('closed')) {
             // open everything greater than it
             for (var i = 0; i <= ix; i++) {
                 $(sibs[i]).removeClass('closed');
             }
         } else {
             // close everything lesser than it
-            for (var i = ix; i <= 2; i++) {
+            for (var i = ix + 1; i <= 2; i++) {
+                if (i == 0) continue;
                 $(sibs[i]).addClass('closed');
             }
         }
     });
 
-    var width  = 805;
-    var height = 480;
+    var width  = $('.svg-container').width();
+    var height = $('.svg-container').height();
     var svgs = d3.selectAll(".svg-container").append("svg")
                                              .attr("width", width)
                                              .attr("height", height);
