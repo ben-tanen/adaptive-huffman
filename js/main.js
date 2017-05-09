f97b7e560ea626a7a5632d9af1fecd3ab0899f43
Ben Tanen <ben.tanen@gmail.com>
2017-05-09 18:30:35 -0400

    added resize notice

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 $(document).ready(function() {
-    $('.mask#fgk').css({
+    $('.mask#middle').css({
         'top': 'calc(' + $('.mask#basic').css('top') + ' + ' + $('.mask#basic').css('padding-bottom') + ' + ' + $('.mask#basic').height() + 'px)'
     });
-    $('body').height(($('.mask#fgk').position()['top'] + $('.mask#fgk').height()) + 'px');
+    
+    $('.mask#fgk').css({
+        'top': 'calc(' + $('.mask#middle').css('top') + ' + ' + $('.mask#middle').css('padding-bottom') + ' + ' + $('.mask#middle').height() + 'px)'
+    });
+
+    $('.mask#end').css({
+        'top': 'calc(' + $('.mask#fgk').css('top') + ' + ' + $('.mask#fgk').css('padding-bottom') + ' + ' + $('.mask#fgk').height() + 'px)'
+    });
+
+    $('body').height(($('.mask#end').position()['top'] + $('.mask#end').height() + 800) + 'px');
 
     init_scrolling();
+
+    $(window).resize(function() {
+        if ($(window).width() < 1010 || $(window).height() < 550) {
+            if ($('#screen-warning').css('display') != "block") screen_warning.open();
+        } else {
+            screen_warning.close();
+        }
+    });
+});
+
+var screen_warning = new jBox('Notice', {
+  content: 'This page is best viewed in a larger window. Try resizing your window!',
+  color: 'black',
+  id: 'screen-warning'
 });
 
 var width  = $('.svg-container').width(),
--- js/scroll.js
+++ js/scroll.js
@@ -3,11 +3,48 @@ function init_scrolling() {
         .sections(d3.selectAll('.section'))
         .offset(20)
         .on('active', function(i){
-            console.log(i + 'th section active');
-
-            if (i == 1) {
-                [n2, t2] = move_node([n2, t2], 50, 50);
+            if (scroll_pos < i) {
+                console.log('scrolling forward:', i);
+                fxns[i]['forward']()
+            } else {
+                console.log('scrolling backward:', i);
+                fxns[i]['backward']()
             }
+
+            scroll_pos = i;
         });
 }
+
+var scroll_pos = 0;
+var none = function() { return; };
+var fxns = [
+    { // header
+        "backward": none,
+        "forward":  none,
+    },{
+        "backward": none,
+        "forward":  none,
+    },{
+        "backward": none,
+        "forward":  none,
+    },{
+        "backward": none,
+        "forward":  none,
+    },{
+        "backward": none,
+        "forward":  none,
+    },{
+        "backward": none,
+        "forward":  none,
+    },{
+        "backward": none,
+        "forward":  none,
+    },{
+        "backward": none,
+        "forward":  none,
+    },{
+        "backward": none,
+        "forward":  none,
+    }
+]
 
