45000e621741341e8892ad00e78679dc4d43a173
Ben Tanen <ben.tanen@gmail.com>
2017-05-10 09:35:15 -0400

    added fixed content table, added auto scroll on refresh

--- js/scroll.js
+++ js/scroll.js
@@ -4,11 +4,13 @@ function init_scrolling() {
         .offset(20)
         .on('active', function(i){
             if (scroll_pos < i) {
-                console.log('scrolling forward:', i);
-                fxns[i]['forward']()
+                for (var j = scroll_pos; j < i; j++) {
+                    console.log('scrolling forward:', j, scroll_pos);
+                    fxns[j]['forward']()
+                }
             } else {
-                console.log('scrolling backward:', i);
-                fxns[i]['backward']()
+                console.log('scrolling backward:', i, scroll_pos);
+                fxns[scroll_pos]['backward']()
             }
 
             scroll_pos = i;
@@ -19,18 +21,7 @@ var scroll_pos = 0;
 var none = function() { return; };
 var fxns = [
     {
-        "backward": function() {
-            svg.selectAll('.node').remove();
-            svg.selectAll('.node-text').remove();
-            svg.selectAll('.edge').remove();
-        },
-        "forward":  none,
-    },{
-        "backward": function() {
-            svg.selectAll('.node').transition().style('opacity', '0');
-            svg.selectAll('.node-text').transition().style('opacity', '0');
-            svg.selectAll('.edge').transition().style('opacity', '0');
-        },
+        "backward": none,
         "forward":  function() {
             var t = trees['bookkeeper'];
             build_tree('test', t['tree'], '', t['height'], t['root-pos'], t['gap-size']);
@@ -39,12 +30,27 @@ var fxns = [
             svg.selectAll('.edge').style('opacity', '0');
         },
     },{
-        "backward": none,
+        "backward": function() {
+            svg.selectAll('.node').remove();
+            svg.selectAll('.node-text').remove();
+            svg.selectAll('.edge').remove();
+        },
         "forward":  function() {
+            $('.fixed-content#freq-table').removeClass('hidden');
+
             svg.selectAll('.node').transition().style('opacity', '1');
             svg.selectAll('.node-text').transition().style('opacity', '1');
             svg.selectAll('.edge').transition().style('opacity', '1');
         },
+    },{
+        "backward": function() {
+            $('.fixed-content#freq-table').addClass('hidden');
+
+            svg.selectAll('.node').transition().style('opacity', '0');
+            svg.selectAll('.node-text').transition().style('opacity', '0');
+            svg.selectAll('.edge').transition().style('opacity', '0');
+        },
+        "forward":  none,
     },{
         "backward": none,
         "forward":  none,
